e1880111b4679442810ab681521e844d647ab807
KaveeshaA28 <tkaveesha272@gmail.com>
2026-06-01 14:36:14 +0530

    Initial commit - Auth module complete

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Smart Employee System API Running" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
